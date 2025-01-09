504693e9c7afe8904d9a62564a1920e7c1e0a8b5
Повалюхин Иван Андреевич <povalyukhin_ia@kampo.local>
2025-01-09 17:31:07 +0300

    Это второй

---
+++ app/Http/Controllers/API/product/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\product;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        return 111111111111111;
+    }
+}
